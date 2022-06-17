80feb6eb4147a86e24cc8ea9e99a2f6344667c67
MCarrillo99 <104209323+MCarrillo99@users.noreply.github.com>
2022-06-17 16:53:42 +0200

    Add files via upload

--- Esercizi_sql/src/JDBC.java
+++ Esercizi_sql/src/JDBC.java
@@ -1,7 +1,6 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JDBC {
     public static void main(String[] args) {
@@ -11,16 +10,37 @@ public class JDBC {
 
         Statement statement = conn.createStatement();
 
+        /**
+         * esercizio 2
+         */
             /*statement.executeUpdate("CREATE TABLE IF NOT EXISTS `persone` (" +
                     "  `student_id` int NOT NULL AUTO_INCREMENT," +
                     "  `last_name` varchar(30) NOT NULL," +
                     "  `first_name` varchar(30) DEFAULT NULL," +
                     "  PRIMARY KEY (`student_id`)" +
                     ") ;");*/
-        statement.executeUpdate("INSERT INTO `newdb`.`persone`" +
+        /*statement.executeUpdate("INSERT INTO `newdb`.`persone`" +
                 "(`last_name`, `first_name`)" +
                 "VALUES" +
-                "(\"Iorio\", \"Maria\");");
+                "(\"Iorio\", \"Maria\");");*/
+
+        /**
+         * esercizio 3
+         */
+        ResultSet res = statement.executeQuery("SELECT * FROM `newdb`.`persone`");
+
+        List<String> surnames = new ArrayList<>();
+
+        while (res.next()) {
+            System.out.println(res.getInt("student_id"));
+            System.out.println(res.getString("last_name"));
+            System.out.println(res.getString("first_name"));
+            System.out.println("------------------");
+            String surname = res.getString("last_name");
+            surnames.add(surname);
+        }
+
+        System.out.println(surnames);
 
     } catch (SQLException exception) {
         exception.printStackTrace();
